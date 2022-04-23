6011f7b1e77c104b229f2ef0184c17a36b60cff2
oka0509 <okaoka1791@gmail.com>
2022-04-23 17:45:35 +0900

    hid

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#8c26&to(46wolcje@tsliab4bwrqc@fy4abf$k-sf#7@p97-3'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
