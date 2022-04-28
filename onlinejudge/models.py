e7e1e2c2e9f94459081f60a51ab757d0d9275172
oka0509 <okaoka1791@gmail.com>
2022-04-28 14:55:25 +0900

    added problem_detail page

--- onlinejudge/models.py
+++ onlinejudge/models.py
@@ -7,6 +7,8 @@ from django.utils import timezone
 class Problem(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    time_limits = models.IntegerField(default=0)
+    memory_limits = models.IntegerField(default=0)
     text = models.TextField()
     constraints = models.TextField()
     inputformat = models.TextField()
@@ -18,8 +20,23 @@ class Problem(models.Model):
     sampleinput3 = models.TextField()
     sampleoutput3 = models.TextField()
     testcases = models.TextField()
+    answers = models.TextField(default='n')
     def publish(self):
         self.save()
 
     def __str__(self):
         return self.title
+
+"""
+class Submission(models.Model):
+    title = models.CharField(max_length=200)
+    code = models.TextField()
+    submitteddate = models.DateTimeField(default=timezone.now)
+    result = models.CharField(max_length=200)
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+"""
--- onlinejudge/views.py
+++ onlinejudge/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Problem
 
-# Create your views here.
+def problem_list(request):
+    problems = Problem.objects.all()
+    return render(request, 'onlinejudge/problem_list.html', {'problems':problems})
+
+def problem_detail(request, pk):
+    problem = get_object_or_404(Problem, pk=pk)
+    return render(request, 'onlinejudge/problem_detail.html', {'problem': problem})
