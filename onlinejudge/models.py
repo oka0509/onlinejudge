e1812cd507e00b1bf51ddf948a85948d7c1ee03f
oka0509 <okaoka1791@gmail.com>
2022-04-29 20:13:55 +0900

    提出フォームの追加，apiに投げて結果を取得する部分

--- onlinejudge/models.py
+++ onlinejudge/models.py
@@ -19,17 +19,28 @@ class Problem(models.Model):
     sampleoutput2 = models.TextField() 
     sampleinput3 = models.TextField()
     sampleoutput3 = models.TextField()
-    testcases = models.TextField()
-    answers = models.TextField(default='n')
+    testinput1=models.TextField(default='n')
+    testoutput1=models.TextField(default='n')
+    testinput2=models.TextField(default='n')
+    testoutput2=models.TextField(default='n')
+    testinput3=models.TextField(default='n')
+    testoutput3=models.TextField(default='n')
+    testinput4=models.TextField(default='n')
+    testoutput4=models.TextField(default='n')
+    testinput5=models.TextField(default='n')
+    testoutput5=models.TextField(default='n')   
     def publish(self):
         self.save()
 
     def __str__(self):
         return self.title
+    def gettestcase(self):
+        list = [[self.testinput1,self.testoutput1], [self.testinput2,self.testoutput2], [self.testinput3,self.testoutput3], [self.testinput4,self.testoutput4], [self.testinput5,self.testoutput5]]
+        return list
 
-"""
 class Submission(models.Model):
     title = models.CharField(max_length=200)
+    language = models.CharField(max_length=200,default='python3')
     code = models.TextField()
     submitteddate = models.DateTimeField(default=timezone.now)
     result = models.CharField(max_length=200)
@@ -39,4 +50,3 @@ class Submission(models.Model):
 
     def __str__(self):
         return self.title
-"""
--- onlinejudge/views.py
+++ onlinejudge/views.py
@@ -1,10 +1,50 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Problem
+from .models import Problem, Submission
+from .form import SubmissionForm
+from django.utils import timezone
+import requests
+import json
+import time
+import sys
 
 def problem_list(request):
     problems = Problem.objects.all()
     return render(request, 'onlinejudge/problem_list.html', {'problems':problems})
 
 def problem_detail(request, pk):
-    problem = get_object_or_404(Problem, pk=pk)
-    return render(request, 'onlinejudge/problem_detail.html', {'problem': problem})
+    if request.method =="POST":
+        form = SubmissionForm(request.POST)
+        if form.is_valid():
+            submission = form.save(commit=False)
+            submission.submitteddate = timezone.now()
+            ml = Problem.objects.get(pk=pk)
+            submission.title = ml.title
+            submission.result = 'wj'
+            caseslist = ml.gettestcase()
+            for i in range(5):
+                url1 = 'http://api.paiza.io/runners/create'
+                query = {
+                    'source_code': submission.code,
+                    'language': submission.language,
+                    'input': caseslist[i][0],
+                    'api_key': 'guest', 
+                    'longpoll': True,
+                }
+                response1 = requests.post(url1, json=query)
+                id = response1.json()['id']
+                params = {
+                    'id': id,
+                    'api_key': 'guest',
+                }
+                url2 = 'http://api.paiza.io/runners/get_details'
+                response2 = requests.get(url2,json=params)
+                result =response2.json()
+                print(result['stdout'])
+            submission.save()
+            #仮のreturn
+            problems = Problem.objects.all()
+            return render(request, 'onlinejudge/problem_list.html', {'problems':problems})
+    else:
+        problem = get_object_or_404(Problem, pk=pk)
+        form = SubmissionForm()
+    return render(request, 'onlinejudge/problem_detail.html', {'problem': problem, 'form' : form})
