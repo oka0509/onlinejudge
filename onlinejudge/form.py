c7a074956fe567f5ab727506ed719b27992798e7
oka0509 <okaoka1791@gmail.com>
2022-05-02 08:38:49 +0900

    提出の選択欄を追加

--- onlinejudge/form.py
+++ onlinejudge/form.py
@@ -3,7 +3,26 @@ from django import forms
 from .models import Submission
 
 class SubmissionForm(forms.ModelForm):
-
+    CHOICE = [
+        ('python3','python3'),
+        ('cpp','cpp'),
+        ('c','c'),
+        ('java','java'),
+        ('csharp','csharp'),
+        ('go','go'),
+        ('ruby','ruby'),
+        ('php','php'),
+        ('rust','rust'),
+        ('javascript','javascript'),
+        ('bash','bash'),
+    ]
+    language = forms.ChoiceField(
+        label='language',
+        required=True,
+        disabled=False,
+        initial=['python3'],
+        choices=CHOICE,
+        widget=forms.Select(attrs={'id': 'one',}))
     class Meta:
         model = Submission
         fields = ('language','code')
